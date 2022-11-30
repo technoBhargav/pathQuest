934a792f05b5b6609743bef7021197fc3ec8ba37
technomarkpratik <pratik.sathawara@technomark.io>
2022-11-30 18:56:02 +0530

    added knowledge center & testimonial

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,33 +18,73 @@ $(document).ready(function () {
     prevArrow: false,
     nextArrow: false,
   });
-
   $('.empower_future').slick({
     centerMode: true,
-    centerPadding: '0px',
-    margin: '0px',
-    slidesToShow: 3,
+    centerPadding: '120px',
+    slidesToShow: 2,
     arrows: true,
+    autoplay: true,
     dots: false,
     nextArrow: '<div class="slick-custom-arrow slick-custom-arrow-right"><img src="/assets/image/next-grey.png" alt=""></div>',
     prevArrow: '<div class="slick-custom-arrow slick-custom-arrow-left"><img src="/assets/image/prev-grey.png" alt=""></div>',
     responsive: [
       {
-        breakpoint: 991,
-        settings: {
-
-          centerMode: true,
-          slidesToShow: 2
-        }
-      },
-      {
         breakpoint: 600,
         settings: {
 
           centerMode: true,
-          slidesToShow: 1
+          slidesToShow: 1,
+          centerPadding: '40px',
         }
       }
     ]
   });
+  $('.testimonial-slider').slick({
+      autoplay: false,
+      autoplaySpeed: 1000,
+      centerPadding: '50px',
+      speed: 600,
+      draggable: true,
+      autoplay: true,
+      infinite: true,
+      asNavFor: '.slider_dots',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: false,
+      fade: true,
+  });
+  $('.slider_dots').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.testimonial-slider',
+    arrows: false,
+    autoplay: true,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    centerPadding: '10px',
+  });
+$(".centername_slider").slick({
+  variableWidth: true,
+  slidesToScroll: 1,
+  asNavFor: ".centerdetail_sider",
+  focusOnSelect: true,
+  arrows: true,
+  autoplay: true,
+  dots: false,
+  infinite: true,
+  nextArrow:
+    '<div class="slick-custom-arrow slick-custom-arrow-right"><img src="/assets/image/blueprev-grey.png" alt=""></div>',
+  prevArrow:
+    '<div class="slick-custom-arrow slick-custom-arrow-left"><img src="/assets/image/bluenext-grey.png" alt=""></div>',
 });
+$(".centerdetail_sider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".centername_slider",
+});
+}); 
